d060140174ec03491060dea266120f1ce9c96fa5
Wesley Cole <wcole@americanbible.org>
2013-08-22 13:23:26 -0400

    Some menu and drop down goodness. Things are looking better!

--- header.php
+++ header.php
@@ -28,10 +28,16 @@
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
-		<a href="#nav" class="menu-toggle"><i class="icon-reorder"></i> Menu</a>
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<nav>
+				<a href="#nav" class="menu-toggle"><i class="icon-reorder"></i> Menu</a>
+				<i class="icon-twitter"></i>
+				<i class="icon-facebook"></i>
+				<i class="icon-instagram"></i>
+				<i class="icon-flickr"></i>
+			</nav>
 		</div>
 	</header><!-- #masthead -->
 
